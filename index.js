dc33fba9da647d875c758c6784ee602482501c8d
nFinityV2 <99304246+nFinityV2@users.noreply.github.com>
2023-01-25 15:08:15 +1100

    Added for loops to apply styles to selected radio buttons

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const feedback = document.getElementById('feedback');
 const cardLayout = document.getElementById('card-layout');
+const btnCircle = document.querySelectorAll('.circle');
+
+// Loops over the circle divs
+for(let i = 0; i < btnCircle.length; i++){
+
+     btnCircle[i].addEventListener('click', function(){
+
+        // Loops over the button to remove the selected button style
+        for(let i = 0; i < btnCircle.length; i++){
+            btnCircle[i].classList.contains('selected');
+            btnCircle[i].classList.remove('selected');
+        }
+        
+        btnCircle[i].classList.add('selected');
+     })
+}
 
 
 feedback.addEventListener('submit', function(e){
     e.preventDefault();
+
+    // Checks which input is selected, and applies to the thank you message
     let rating = document.querySelector('input[name="rate"]:checked').value;
 
     cardLayout.innerHTML = `
